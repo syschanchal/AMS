62ad6dde62ed00ae4847e6eac6dc719f3788d8f0
DEBASIS PAUL®™ (Zhýralý Pāvils) #BlackLivesStillMatter <idebasispaul@gmail.com>
2022-03-21 14:52:59 +0600

    Add project files.

---
+++ AMS/Models/AMSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public class AMSDbContext : DbContext
+    {
+        public AMSDbContext() : base("DBConnectionStr")
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ AMS/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int Age { get; set; }
+    }
+}
